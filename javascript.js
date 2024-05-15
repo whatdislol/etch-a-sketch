03d8088c95c8c4022123e2ea29b6b2f726889a59
whatdislol <neunglert@gmail.com>
2024-05-15 16:02:41 +0700

    Add resize button

--- javascript.js
+++ javascript.js
@@ -1,4 +1,7 @@
 const container = document.querySelector(".container");
+const resizeButton = document.createElement("button");
+const body = document.querySelector("body");
+
 function createCanvas(size){
     for(let i = 0; i < size; i++){
         const row = document.createElement("div");
@@ -11,13 +14,29 @@ function createCanvas(size){
         container.appendChild(row);
     }
 }
+function clearCanvas(){
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
+    }
+}
 function paint(event){
     const target = event.target;
     if(target.classList.contains("pixel") && event.buttons === 1){
         target.style.backgroundColor = "blue";
     }
 }
+resizeButton.textContent = "Resize";
+
 container.addEventListener("mousedown", paint);
 container.addEventListener("mouseover", paint);
+resizeButton.addEventListener("click", () => {
+    const userInput = +prompt('Enter grid size');
+    if(typeof(userInput) === "number" && userInput > 0){
+        clearCanvas();
+        createCanvas(userInput);
+    }
+});
+
+body.appendChild(resizeButton)
 
 createCanvas(16);
